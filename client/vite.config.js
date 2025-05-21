0eff51e3440ed4ffcc6e49ea174ada1b1e6138b6
Abenezer365 <abenezerkibebe@gmail.com>
2025-05-21 20:07:17 +0300

    Config 8

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    base: env.NODE_ENV === 'production' ? '/your-repo-name/' : '/',
+    base: env.NODE_ENV === 'production' ? '/amazon-clone/' : '/',
     plugins: [react()]
   }
 })
